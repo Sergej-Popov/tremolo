256d318472b65ffd82b775fb890cab55ee682209
Sergej <sergejpopov@outlook.com>
2025-06-22 18:49:09 +0100

    bundle highlight.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import hljs from 'highlight.js/lib/common'
+import hljsLineNumbers from 'highlightjs-line-numbers.js'
+import 'highlight.js/styles/default.css'
+import 'highlightjs-line-numbers.js/dist/highlightjs-line-numbers.css'
+
+hljsLineNumbers(hljs)
+(window as any).hljs = hljs
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
